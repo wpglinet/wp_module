1acf5dc5589e7af183fd065127e6633133e5587a
wpglinet <pan.wei@gl-inet.com>
2025-12-26 16:01:53 +0800

    Update wp-simple-init.sh

--- wp-openwrt-feed/packages/utils/wp_module/files/wp-simple-init.sh
+++ wp-openwrt-feed/packages/utils/wp_module/files/wp-simple-init.sh
@@ -10,11 +10,11 @@ uci set wireless.default_radio1.ssid='gl-inet-5G'
 uci commit wireless
 
 # 2........password
-echo "2. ......root......... goodlife..."
+echo "2. 设置root密码为goodlife..."
 if echo -e "goodlife\ngoodlife" | passwd root >/dev/null 2>&1; then
         echo ".................."
 else
-        echo "......................................."
+        echo "密码设置失败，使用备用方法,利用哈希值"
         sed -i 's/root:.*:/root:$1$wEehtjxj$YBu1quNfMkje5EF9wzh9q0:0:0:99999:7:::/' /etc/shadow 2>/dev/null
 fi
 
